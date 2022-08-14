3d0598b3939754a951597a58d8fe480b7edfd8ac
Rohan Singh <rohan.2010.2000.rs@gmail.com>
2022-08-14 20:42:53 +0530

    Web Scraper Telegram Bot is Finally Ready!

--- src/main/java/in/rohan/webscraper/TelegramNotificationBot.java
+++ src/main/java/in/rohan/webscraper/TelegramNotificationBot.java
@@ -8,12 +8,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
-import java.util.concurrent.CompletableFuture;
-
 public class TelegramNotificationBot extends TelegramLongPollingBot {
 
-    String chatID = "845726056";
+    String chatID = ConfigManager.getKeys("CHAT_ID");
 
 
     @Override
@@ -34,15 +31,12 @@ public class TelegramNotificationBot extends TelegramLongPollingBot {
 
             String userMessage = update.getMessage().getText();
             User user = update.getMessage().getFrom();
-//            String chatID = update.getMessage().getChatId().toString();  // for storing in database
 
             String reply = "_";
 
             if (userMessage.equals("/start")) {
                 reply = "Welcome, " + user.getFirstName();
 
-                //Performing Async DB request  (sync causes  the reply to be  sent late)
-//                CompletableFuture.supplyAsync(() -> DatabaseManager.addUser(chatID, user.getFirstName()));
             }
 
             SendMessage message = new SendMessage();
@@ -59,57 +53,20 @@ public class TelegramNotificationBot extends TelegramLongPollingBot {
 
     public void sendToUser(String messageString) {
 
-//        ArrayList<String> chatIDs = DatabaseManager.getChatIDs();
-
-
         SendMessage message = new SendMessage();
         message.setChatId(chatID);
         message.setText(messageString);
 
         try {
             execute(message);
-//            Thread.sleep(1000); // to avoid Telegram Bot block (due to flooding).
         }
         catch (TelegramApiException e) {
-            if (e.toString().contains("[403]")) { // user stopped the bot
+            if (e.toString().contains("[403]")) {              // user stopped the bot
                 System.out.println("User Stopped the Bot");
-//                CompletableFuture.supplyAsync(() -> DatabaseManager.deleteUser(id));
             } else {
                 System.err.println(">>>COULD NOT SEND POSTS TO " + chatID);
                 e.printStackTrace();
             }
         }
     }
-
-
-    /**
-     * Sends the given string to all users.
-     * @param messageString messageString
-     */
-//    public void sendToAllUsers(String messageString) {
-//
-//        ArrayList<String> chatIDs = DatabaseManager.getChatIDs();
-//
-//        for (String id : chatIDs) {
-//            SendMessage message = new SendMessage();
-//            message.setChatId(id);
-//            message.setText(messageString);
-//            message.setParseMode("html");
-//
-//            try {
-//                execute(message);
-//                Thread.sleep(1000); // to avoid Telegram Bot block (due to flooding).
-//            }
-//            catch (TelegramApiException | InterruptedException e) {
-//                if (e.toString().contains("[403]")) { // user stopped the bot
-////                    System.out.println("User Stopped the Bot");
-//                    CompletableFuture.supplyAsync(() -> DatabaseManager.deleteUser(id));
-//                }
-//                else {
-//                    System.err.println(">>>COULD NOT SEND POSTS TO " + id);
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
 }
